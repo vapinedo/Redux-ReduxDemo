1173c7ac679d06820b7f82388502c9f1e2e7431e
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-30 13:23:43 -0500

    [ADD] Reducers

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function buyCake() {
     }
 }
 
+// (previousState, action) => newState
+
+const initialState = {
+    numberOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes - 1
+            }
+        default: return state; 
+    }
+}
