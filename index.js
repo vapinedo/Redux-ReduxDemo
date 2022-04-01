6816529b7f0d1da1a305eebadaea07a1441f97b4
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-01 15:59:49 -0500

    [ADD] Multiple Reducers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require('redux')
 const createStore = redux.createStore
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
     return {
@@ -8,19 +9,56 @@ function buyCake() {
         info: 'First redux action'
     }
 }
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
 
 // (previousState, action) => newState
 
-const initialState = {
-    numberOfCakes: 10
+// const initialState = {
+//     numberOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+const initialCakeState = {
+    numberOfCakes: 10,
+}
+const initialIceCreamState = {
+    numOfIceCreams: 20
 }
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case BUY_CAKE:
+//             return {
+//                 ...state,
+//                 numberOfCakes: state.numberOfCakes - 1
+//             }
+//         case BUY_ICECREAM:
+//             return {
+//                 ...state,
+//                 numOfIceCreams: state.numOfIceCreams - 1
+//             }            
+//         default: return state; 
+//     }
+// }
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE:
             return {
                 ...state,
                 numberOfCakes: state.numberOfCakes - 1
+            }
+        default: return state; 
+    }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1
             }
         default: return state; 
     }
@@ -32,4 +70,6 @@ const unsubscribe = store.subscribe(() => console.log('Update state', store.getS
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
